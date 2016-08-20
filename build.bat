04b7f8a6d2711b247bca26af8b179b7c01a5f4da
Christopher Fredericks <cfredmakecode+git@gmail.com>
2016-08-20 15:46:13 -0400

    added stb_image to load alpha-containing PNGs easier, separated clouds out preparing to animate

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-REM C:\Emscripten>emcc w:\loltap\loltap.cpp -o w:\loltap\loltap.html emscripten\1.35.0\system\lib\libSDL2.a
+REM W:\loltap>emcc w:\loltap\loltap.cpp -o w:\loltap\loltap.html -Iinclude --embed-file bg.bmp --embed-file clouds.png c:\emscripten\emscripten\1.35.0\system\lib\libSDL2.a
 cl loltap.cpp /nologo /Zi /MT /Iinclude lib\sdl2\x64\sdl2.lib /link /subsystem:console
--- loltap.cpp
+++ loltap.cpp
@@ -1,3 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
@@ -11,6 +13,7 @@ typedef struct game_state {
   uint32_t ticks;
   SDL_Renderer *sdlRenderer;
   SDL_Window *sdlWindow;
+  SDL_Texture *bg;
   bool32 running;
   struct {
     int x, y;
@@ -18,13 +21,17 @@ typedef struct game_state {
   int frames;
   uint32_t lastMs;
   uint32_t lastFPSstamp;
+  struct {
+    SDL_Texture *tex;
+    SDL_Rect *rect;
+  } clouds;
 } game_state;
 
 void emscripten_loop_workaround(void *gs);
 void main_loop(game_state *gs);
 
-#define SCREEN_H 720
-#define SCREEN_W 1280
+#define SCREEN_H 640
+#define SCREEN_W 640
 #define MS_PER_TICK 1000 / 60
 
 void handle_events(game_state *gs) {
@@ -62,7 +69,53 @@ void handle_events(game_state *gs) {
   }
 }
 
-#undef main
+#undef main // to shut up winders
+
+bool32 load_texture_from_bitmap(SDL_Renderer *renderer, SDL_Texture **tex,
+                                const char *name) {
+  // it's just easier to get it converted to a texture this way, even though
+  // there's the extra step;
+  // todo i think we leak memory if sdl_freesurface doesn't call free() on
+  // underlying pixel data..
+  int x, y, n;
+  unsigned char *data = stbi_load(name, &x, &y, &n, 4);
+  if (!data) {
+    printf("failed to load %s!\n", name);
+    return false;
+  }
+  SDL_Surface *temp = SDL_CreateRGBSurfaceFrom(
+      data, x, y, n * 8, 4 * x, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+  // SDL_Surface *temp = SDL_LoadBMP(name);
+  if (temp == 0) {
+    printf("failed to load %s!\n", name);
+    printf("%s\n", SDL_GetError());
+    return false;
+  }
+  // SDL_SetColorKey(temp, true, 0xFFFF00FF);
+  *tex = SDL_CreateTextureFromSurface(renderer, temp);
+  if (*tex == 0) {
+    printf("failed to convert %s into texture!\n", name);
+    SDL_FreeSurface(temp);
+    return false;
+  }
+  SDL_FreeSurface(temp);
+  return true;
+}
+
+bool32 load_bitmaps(game_state *gs) {
+  if (!load_texture_from_bitmap(gs->sdlRenderer, &gs->bg, "bg.bmp")) {
+    SDL_Quit();
+    return false;
+  }
+  printf("loaded bg!\n");
+  if (!load_texture_from_bitmap(gs->sdlRenderer, &gs->clouds.tex,
+                                "clouds.png")) {
+    SDL_Quit();
+    return false;
+  }
+  printf("loaded clouds!\n");
+  return true;
+}
 
 extern "C" int main(int argc, char **argv) {
   game_state gs = {0};
@@ -84,9 +137,16 @@ extern "C" int main(int argc, char **argv) {
                               &gs.sdlRenderer);
   SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
   SDL_RenderSetLogicalSize(gs.sdlRenderer, 32, 32);
-  printf("set 32x32 rendering res\n");
 #ifdef __EMSCRIPTEN__
   emscripten_cancel_main_loop();
+#endif
+  printf("set 32x32 rendering res\n");
+  if (!load_bitmaps(&gs)) {
+    printf("bailing!\n");
+    return 1;
+  }
+
+#ifdef __EMSCRIPTEN__
   emscripten_set_main_loop_arg(&emscripten_loop_workaround, (void *)&gs, 0, 1);
 #else
   while (gs.running) {
@@ -109,9 +169,11 @@ void main_loop(game_state *gs) {
   gs->ticks++;
   /// todo handle having to step multiple ticks because fps got behind
   handle_events(gs);
-  SDL_SetRenderDrawColor(gs->sdlRenderer, gs->mouse.x, gs->ticks, gs->mouse.y,
-                         255);
+  SDL_SetRenderDrawColor(gs->sdlRenderer, 0, 0, 0, 255);
   SDL_RenderClear(gs->sdlRenderer);
+  SDL_RenderCopy(gs->sdlRenderer, gs->bg, 0, 0);
+  SDL_RenderCopy(gs->sdlRenderer, gs->clouds.tex, 0, 0);
+
   SDL_RenderPresent(gs->sdlRenderer);
   gs->frames++;
   if (SDL_GetTicks() - gs->lastFPSstamp > 1000) {
